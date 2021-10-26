505c5df7884aea7f3e0a85f6f5aab339946c86f4
Renan <renan.nunes@mblabs.com.br>
2021-10-25 22:23:15 -0300

    Base project

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (request, response)=>{
+  response.json({message: "Hello word"})
+})
+
+app.listen(8000, ()=>{
+  console.log("Server running")
+})
