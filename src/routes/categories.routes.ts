5b18f5b81092ac69b234138351b40ee0dc4d14dd
Renan <renan.sergipe68@gmail.com>
2021-10-27 14:37:33 -0300

    Refactor: Utilizando o Princípio de Responsabilidade Única (SRP)

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { CategoriesRepository } from '../repositories/CategoriesRepository'
+import { CreateCategoryService } from '../services/CreateCategoryService'
 
 
 const categoriesRoutes = Router()
@@ -8,13 +9,8 @@ const categoriesRepository = new CategoriesRepository()
 categoriesRoutes.post("/categories", (request, response)=>{
   const { name, description } = request.body
 
-  const alreadyExists = categoriesRepository.findByName(name)
-
-  if(alreadyExists){
-    return response.status(400).json({message: "Category already exists"})
-  }
-
-  categoriesRepository.create({ name, description })
+  const createCategoryService = new CreateCategoryService(categoriesRepository)
+  createCategoryService.execute({ name, description })
   return response.status(201).send()
 })
 
---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,22 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository"
+
+interface IRequest {
+  name: string
+  description: string
+}
+
+class CreateCategoryService {
+
+  constructor( private categoriesRepository: CategoriesRepository ){}
+
+  execute({ name, description }: IRequest){
+
+    const alreadyExists = this.categoriesRepository.findByName(name)
+    if(alreadyExists){
+      throw new Error("Category already exists")
+    }
+    this.categoriesRepository.create({ name, description })
+  }
+}
+
+export { CreateCategoryService }
