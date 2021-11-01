2df326b0839167c0172cd510e00fbe34be05ff11
Renan <renan.sergipe68@gmail.com>
2021-11-01 15:58:52 -0300

    Feat: configurando swagger para documentaçao

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import { router } from './routes'
+import swaggerUi from 'swagger-ui-express'
+import SwaggerFile from  './swagger.json'
 
 const app = express()
 
+
 app.use(express.json())
 app.use(router)
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(SwaggerFile))
 
 app.get("/", (request, response)=>{
   response.json({message: "Application running"})
