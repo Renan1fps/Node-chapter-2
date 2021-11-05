413d07bcfbf192798828bd70aabe42716a483274
Renan <renan.sergipe68@gmail.com>
2021-11-05 20:49:31 -0300

    Refactor: resolvendo bug de migration nao rodar no banco

--- src/database/migrations/1636155611410-CreateCategories.ts
+++ src/database/migrations/1636155611410-CreateCategories.ts
@@ -1,6 +1,6 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class CreateCategories1636026150452 implements MigrationInterface {
+export class CreateCategories1636155611410 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable( new Table({
